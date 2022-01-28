ef3e903d3bd2288471c24ca70be7b5330b0304c8
venezia-dev <dev.venezia@gmail.com>
2022-01-27 22:32:42 -0300

    Changes.

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,8 +6,7 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-knobs/register",
-    "@storybook/addon-actions",
+    "@storybook/addon-actions"
   ],
   "framework": "@storybook/react"
 }
